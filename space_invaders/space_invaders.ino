d81766e8b89c7a038f460a169cb3d4fba4ff2f03
Crystal Sawers <sawece1@student.op.ac.nz>
2023-06-07 09:47:45 +1200

    add libraries and research ld

--- space_invaders/space_invaders.ino
+++ space_invaders/space_invaders.ino
@@ -2,9 +2,11 @@
 #include <U8g2lib.h>
 #include <U8x8lib.h>
 
+U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ U8X8_PIN_NONE);
+
 void setup() {
   // put your setup code here, to run once:
-
+  u8g2.begin();
 }
 
 void loop() {

01c9961079f4ef9c082b741a4832501f3ff6d147
Crystalsawers <sawece1@student.op.ac.nz>
2023-06-11 17:12:02 +1200

    edit pins used for specific library

--- space_invaders/space_invaders.ino
+++ space_invaders/space_invaders.ino
@@ -2,8 +2,12 @@
 #include <U8g2lib.h>
 #include <U8x8lib.h>
 
-U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ U8X8_PIN_NONE);
+#include <MUIU8g2.h>
+#include <U8g2lib.h>
+#include <U8x8lib.h>
 
+// U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ U8X8_PIN_NONE);
+U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
 void setup() {
   // put your setup code here, to run once:
   u8g2.begin();

a532a93bdd52b04ead99db4a26e5000bb7783568
Crystal Sawers <sawece1@student.op.ac.nz>
2023-06-14 09:39:00 +1200

    display aliens and mothership

--- space_invaders/space_invaders.ino
+++ space_invaders/space_invaders.ino
@@ -5,54 +5,40 @@
 U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/13, /* data=*/11, /* CS=*/10, /* reset=*/8);
 Aliens aliens(u8g2, 8, 4, 2, 6);
 
-
 void setup() {
   // put your setup code here, to run once:
   u8g2.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  alienFunction();
-  Mothership();
-  delay(10000);
+
+  drawAliensAndMothership();
+  delay(500);
 }
 
-void alienFunction() {
-
+void drawAliensAndMothership() {
   u8g2.firstPage();
+
   do {
     u8g2.clearBuffer();
 
+    // Draw the aliens
     aliens.update();
     aliens.draw();
 
-  } while (u8g2.nextPage());
+    // Draw the mothership
+    byte mothershipBytes[] = {
+      B00010000,  // Row 1
+      B00010000,  // Row 2
+      B00111000,  // Row 3
+      B00111000,  // Row 4
+      B01111100,  // Row 5
+      B01111110,  // Row 6
+      B11011011,  // Row 7
+      B11011011   // Row 8
+    };
 
-  delay(100);
-}
-
-void Mothership() {
-
-// Draw mothership
-byte mothershipBytes[] = {
-  B00010000,  // Row 1
-  B00010000,  // Row 2
-  B00111000,  // Row 3
-  B00111000,  // Row 4
-  B01111100,  // Row 5
-  B01111110,  // Row 6
-  B11011011,  // Row 7
-  B11011011   // Row 8
-};
-
-  u8g2.firstPage();
-
-  do {
-    u8g2.clearBuffer();
-  
-        // Draw the mothership pattern
-     for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) {
       for (int j = 0; j < 8; j++) {
         byte row = mothershipBytes[i];
         if (row & (1 << (7 - j))) {

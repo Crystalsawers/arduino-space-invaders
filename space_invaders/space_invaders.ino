40d56bbbd1f655a29c88502ad1e18f6a2c3eb0b2
Crystalsawers <sawece1@student.op.ac.nz>
2023-06-13 19:21:26 +1200

    add custom library for aliens

--- space_invaders/space_invaders.ino
+++ space_invaders/space_invaders.ino
@@ -1,13 +1,13 @@
-#include <MUIU8g2.h>
 #include <U8g2lib.h>
-#include <U8x8lib.h>
-
-#include <MUIU8g2.h>
 #include <U8g2lib.h>
-#include <U8x8lib.h>
 
-// U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ U8X8_PIN_NONE);
-U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* CS=*/ 10, /* reset=*/ 8);
+// Custom libraries
+#include <Aliens.h>
+
+
+U8G2_ST7920_128X64_F_SW_SPI u8g2(U8G2_R0, /* clock=*/13, /* data=*/11, /* CS=*/10, /* reset=*/8);
+Aliens aliens(u8g2, 8, 4, 2, 6);
+
 void setup() {
   // put your setup code here, to run once:
   u8g2.begin();
@@ -15,5 +15,19 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  alienFunction();
+}
 
+void alienFunction() {
+
+  u8g2.firstPage();
+  do {
+    u8g2.clearBuffer();
+
+    aliens.update();
+    aliens.draw();
+
+  } while (u8g2.nextPage());
+
+  delay(100);
 }
